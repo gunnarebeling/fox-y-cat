a75786559b2c8856aad820db4e59f5e29340a305
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-08-14 15:27:11 -0500

    second call

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,18 @@ fetch("https://randomfox.ca/floof/")
     const foxImageElement =document.querySelector('#fox');
     foxImageElement.src = convertedData.image;
 })
+// Define an asynchronous function
+const getTheDog = async () => {
+    // Request the data and wait for the data to come back over the Web
+    const response = await fetch("https://random.dog/woof.json")
+
+    // Parse the received JSON string into an Object
+    const dogJSONConvertedToObject = await response.json()
+
+    // Target the HTML image element
+    const dogImgElement = document.querySelector("#dog")
+
+    // Update the `src` with a URL. The browser will request that image.
+    dogImgElement.src = dogJSONConvertedToObject.url
+}
+getTheDog()
